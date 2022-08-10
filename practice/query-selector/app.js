a798e482a1415245eeb010c27318dd40d5f803ab
Paul D'Amico <pauldamico88@gmail.com>
2022-08-10 14:04:43 -0400

    updated query-selector

---
+++ practice/query-selector/app.js
@@ -0,0 +1,7 @@
+let title = document.querySelectorAll("h1")
+let list = document.querySelectorAll("li#list")
+
+for(let x =0; x < title.length; x++){
+title[x].textContent = "New Title " + x}
+
+list[0].textContent = "New List 1"
