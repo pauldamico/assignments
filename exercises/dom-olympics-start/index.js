f42edf1709921f28014a2f73a9d5815d635e0010
Paul D'Amico <pauldamico88@gmail.com>
2022-08-11 16:32:43 -0400

    Finished dom olympics.  Did not complete extra credit

--- exercises/dom-olympics-start/index.js
+++ exercises/dom-olympics-start/index.js
@@ -1,13 +1,53 @@
-const header = document.getElementById("header")
-const title = document.createElement("h1")
-const subtitle1 = document.createElement("span")
-const subtitle2 = document.createElement("span")
+const header = document.getElementById("header");
+const title = document.createElement("h1");
+const subTitle = document.createElement("h2");
+const messageLeft = document.getElementsByClassName("message left");
+const messageRight = document.getElementsByClassName("message right");
+const dropDown = document.querySelector("#theme-drop-down");
+const form = document.getElementsByName("message");
+const messages = document.getElementById("messages");
+const input = document.getElementById("input");
+const userText = document.createElement("div");
+input.value = "";
+messageLeft[0].textContent = "How was the game last night";
+messageLeft[1].textContent = "Nicee!!! Next time I will have to come";
+messageRight[0].textContent = "It was Awesome!";
+messageRight[1].textContent = "I Agree!";
 
-title.textContent = "JavaScript Made This!!"
-title.classList.add("header")
-subtitle1.textContent = "Paul"
-subtitle1.classList.add("name")
-subtitle2.textContent = " wrote the JavaScript"
-header.append(title, subtitle1, subtitle2)
+dropDown.addEventListener("click", function () {
+  for (let x = 0; x < messageLeft.length; x++)
+    if (dropDown.value === "theme-one") {
+      messageLeft[x].style.backgroundColor = "blue";
+      messageLeft[x].style.color = "white";
+    } else if (dropDown.value === "theme-two") {
+      messageLeft[x].style.backgroundColor = "red";
+    }
+  for (let x = 0; x < messageRight.length; x++) {
+    if (dropDown.value === "theme-one") {
+      messageRight[x].style.backgroundColor = "brown";
+    } else if (dropDown.value === "theme-two") {
+      messageRight[x].style.backgroundColor = "black";
+      messageRight[x].style.color = "white";
+    }
+  }
+});
 
-console.log(header)
+form[0].addEventListener("submit", test);
+
+function test(e) {
+  e.preventDefault();
+
+userText.classList.add("message")
+userText.classList.add("left")
+  userText.innerHTML += `<div>${input.value}</div>`
+
+  messages.append(userText);
+  input.value = "";
+}
+
+title.textContent = "JavaScript Made This!!";
+title.classList.add("header");
+subTitle.innerHTML = `<span class="name">Paul</span><span> wrote this JavaScript</span>`;
+subTitle.style.textAlign = "center";
+
+header.append(title, subTitle);
