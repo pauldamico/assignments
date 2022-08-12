f77145b50edbd5078a96852d989adc5fa951ee8e
Paul D'Amico <pauldamico88@gmail.com>
2022-08-12 13:24:39 -0400

    Finished insert-whitespace challenge

---
+++ challenge/insert-whitespace/app.js
@@ -0,0 +1,14 @@
+const str = "SheWalksToTheBeach";
+
+function insertWhiteSpace(str) {
+  let strArray = str.split("")
+  let capString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let capArray = capString.split("")
+for(let x = 0; x<strArray.length; x++){
+for(let i = 1; i < capArray.length; i++)
+if (strArray[x] === capArray[i]){ strArray[x -1] += " "}
+}
+return strArray.join("")
+}
+
+console.log(insertWhiteSpace(str));
