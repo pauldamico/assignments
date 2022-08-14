a098e3017f798bfe610f5df246d2b1d25e7f6f5f
Paul D'Amico <pauldamico88@gmail.com>
2022-08-14 14:48:44 -0400

    removed local storage functionality.  Could not get local storage to save my edit button change values. it currently just console logs localstorage

--- projects/moms-shopping-list-start/index.js
+++ projects/moms-shopping-list-start/index.js
@@ -1,68 +1,79 @@
 let ul = document.getElementById("ul");
 let itemValue = document.addItem.title.value;
-let newValue = JSON.parse(localStorage.getItem("newValue")) || []
-clearLocalStore()
+let listArray = JSON.parse(localStorage.getItem("listArray")) || [];
+
+clearLocalStore();
+
 renderItems();
 
 //submit and save to local storage (local storage does not work right now)
 function renderItems() {
+ 
   document.addItem.addEventListener("submit", function (event) {
-    event.preventDefault();        
-    newValue.push(document.addItem.title.value);
-    localStorage.setItem("newValue", JSON.stringify(newValue));
-  
+    event.preventDefault();
+   listArray.push(document.addItem.title.value);
+   localStorage.setItem("listArray", JSON.stringify(listArray))
+   // ;
     document.addItem.title.value = "";
-    listItems(newValue);
-    console.log(localStorage);
-
+    console.log()
+    listItems();
+    
+    console.log(localStorage)
   });
+
 
 }
 
-
 //shows the list items after submit is clicked
-function listItems(userInputArray) {
+function listItems() {
   let li = document.createElement("li");
-  for (let x = 0; x < userInputArray.length; x++) {
+  for (let x = 0; x < listArray.length; x++) {
+ 
     li.id = "li";
-    li.className="list"
+    li.className = "list";
     document.getElementById("li");
     document.getElementById("removeBut");
-    li.textContent = userInputArray[x];
+    li.textContent = listArray[x]
     ul.append(li);
+    console.log();
+    addEditButton(li);
+    addRemoveButton(li);
+    
   }
-  editItemName(li);
-  removeItem(li);
-  }
 
-
-
-
+}
 
 // adds the edit function and allows you to change the text value
-function editItemName(li) {
+function addEditButton(li) {
   let editBut = document.createElement("button");
-  editBut.className="edit"
+  editBut.className = "edit";
   editBut.textContent = "Edit";
   editBut.addEventListener("click", () => {
-    let newInput = document.createElement("input");
-    newInput.className="editinput"
-    editBut.parentElement.append(newInput);
-    editBut.textContent = "Save";
+    let editNewInputChild = document.createElement("input");
+    editNewInputChild.value = li.firstChild.data    
+    editNewInputChild.className = "editinput";   
+
+    //let editInput = document.createElement("editinput");
+    //editNewInputChild.value = editInput.parentElement.t;
+    editBut.parentElement.append(editNewInputChild);
+    editBut.textContent = "save"
+    
     editBut.addEventListener("click", () => {
-      editBut.parentElement.textContent = newInput.value;
+      editNewInputChild.parentElement.textContent = editNewInputChild.value;
+  
       editBut.textContent = "Edit";
-      editBut.addEventListener("click", editItemName(li));
-      removeItem(li);
+      editBut.addEventListener("click", addEditButton(li));
+      addRemoveButton(li);
+      console.log(listArray)
     });
   });
 
   li.appendChild(editBut);
- 
+  
 }
 
 // adds the remove (X) button which removes the item from the list
-function removeItem(li) {
+function addRemoveButton(li) {
   let removeButton = document.createElement("button");
   removeButton.className = "removebutton";
   removeButton.textContent = "X";
@@ -70,10 +81,11 @@ function removeItem(li) {
     removeButton.parentElement.remove()
   );
   li.appendChild(removeButton);
+
 }
 
 //Button to clear Local storage
-function clearLocalStore(){
+function clearLocalStore() {
   let clearButton = document.createElement("button");
   clearButton.addEventListener("click", () => localStorage.clear());
   clearButton.textContent = "Clear Local Storage";
