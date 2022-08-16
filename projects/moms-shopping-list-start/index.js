dc0add1dc7cc8092e19adbd12313d77cec9fbe37
Paul D'Amico <pauldamico88@gmail.com>
2022-08-16 00:40:31 -0400

    updated comments for moms-shopping-list

--- projects/moms-shopping-list-start/index.js
+++ projects/moms-shopping-list-start/index.js
@@ -1,35 +1,34 @@
 let ul = document.getElementById("ul");
-let itemValue = document.addItem.title.value;
 let listArray = JSON.parse(localStorage.getItem("listArray")) || []; //fix this
-let updatedList = []
+//let itemValue = document.addItem.title.value;
+
+//let updatedList = [] //not currently in use
 
 clearLocalStore();
 renderItems();
 
-//submit and save to local storage (local storage does not work right now)
+
 function renderItems() {
-  document.addItem.addEventListener("submit", function (event) {
-    event.preventDefault();
-    listArray.push(document.addItem.title.value);
-    localStorage.setItem("listArray", JSON.stringify(listArray)); //changes listArray to a string and pushes to Local Storage
-    document.addItem.title.value = "";
-    listItems();
+  document.addItem.addEventListener("submit", function (event) {  //submit button event
+    event.preventDefault();                                       // prevents page from refreshing
+    listArray.push(document.addItem.title.value);                 //pushes the main input values to the list array
+    localStorage.setItem("listArray", JSON.stringify(listArray)); //saves list array to Local Storage
+    document.addItem.title.value = "";                            //sets value back to a blank string
+    listItems();                                                  //calls the listItems function
    });
 }
 
 
-//shows the list items after submit is clicked
+//shows the list items from list Arrayafter submit is clicked
 function listItems() {
-  let li = document.createElement("li");
-  for (let x = 0; x < listArray.length; x++) {
-    li.id = "li";
-    li.className = "list";
-    document.getElementById("li");
-    document.getElementById("removeBut");
-    li.textContent = listArray[x];
-    ul.append(li);
-    addEditButton(li);
-    addRemoveButton(li);
+  let li = document.createElement("li");         // creates list element and assigns it to li
+  for (let x = 0; x < listArray.length; x++) {   //for loop for listArray items
+    li.id = "li";                                //sets <li id="li">
+    li.className = "list";                       //sets the class for all the li elements
+    li.textContent = listArray[x];               //applies the strings from all the listArray indexs to seperate li.textcontents
+    ul.append(li);                               //appends each li item and displays the list
+    addEditButton(li);                           //adds edit button to each list item (calls addEditButton function)
+    addRemoveButton(li);                         //adds removebutton (X) to each list item (calls addRemoveButton function)
 
   }
   console.log(localStorage)
@@ -37,21 +36,20 @@ function listItems() {
 
 // adds the edit function and allows you to change the text value
 function addEditButton(li) {
-  let editBut = document.createElement("button");
-  editBut.className = "edit";
-  editBut.textContent = "Edit";
-  editBut.addEventListener("click", () => {
-    let editNewInputChild = document.createElement("input");
-    editNewInputChild.value = li.firstChild.data;
-    editNewInputChild.className = "editinput";
-    li.appendChild(editNewInputChild);
-    editBut.textContent = "save";
-    editBut.addEventListener("click", () => {
-    editNewInputChild.parentElement.textContent = editNewInputChild.value;
- 
-     editBut.textContent = "Edit";
-     editBut.addEventListener("click", addEditButton(li));
-      addRemoveButton(li);
+  let editBut = document.createElement("button");                              //creates the edit button element
+  editBut.className = "edit";                                                  //sets class to edit
+  editBut.textContent = "Edit";                                                //names the button edit
+  editBut.addEventListener("click", () => {                                    //adds eventlistener for when edit is clicked
+    let editNewInputChild = document.createElement("input");                   //creates a new inputfield element
+    editNewInputChild.value = li.firstChild.data;                              //prefills the inputfield to the orginal li value
+    editNewInputChild.className = "editinput";                                 //sets class to editinput
+    li.appendChild(editNewInputChild);                                         //adds/shows the new inputfield element
+    editBut.textContent = "save";                                              //changes the edit textContent to "save"
+    editBut.addEventListener("click", () => {                                  //adds a eventListener for when save is clicked
+    editNewInputChild.parentElement.textContent = editNewInputChild.value;     //updates the orginal text value to the edited value
+     editBut.textContent = "Edit";                                             //sets the button textContent back to "Edit"
+     editBut.addEventListener("click", addEditButton(li));                     //eventListener to Loop the addEditButton function if clicked again
+      addRemoveButton(li);                                                     //adds the RemoveButton back on
       //for(let i =0; i< ul.children.length; i++){
       //  updatedList.push(ul.children[i].textContent);   
     //}  
@@ -65,7 +63,7 @@ function addEditButton(li) {
   li.appendChild(editBut);
 }
 
-// adds the remove (X) button which removes the item from the list
+// adds the remove (X) button which removes the remove button's parent element thus also removing the child elements.
 function addRemoveButton(li) {
   let removeButton = document.createElement("button");
   removeButton.className = "removebutton";
