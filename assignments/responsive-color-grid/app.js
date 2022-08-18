929dd3f470fa9be114a7c40446143d5c9fbb5783
Paul D'Amico <pauldamico88@gmail.com>
2022-08-18 11:32:27 -0400

    fixed responsive color grid

---
+++ assignments/responsive-color-grid/app.js
@@ -0,0 +1,40 @@
+let mainDiv = document.createElement("div")
+let colorDiv = document.createElement("div")
+let pinkDiv = document.createElement("div")
+let pink1 = document.createElement("section")
+let purpleDiv = document.createElement("div")
+let purple = document.createElement("span")
+let purple2 = document.createElement("span")
+let purple3 = document.createElement("span")
+let purple4 = document.createElement("span")
+let blueDiv = document.createElement("div")
+let blue = document.createElement("section")
+let blue2 = document.createElement("section")
+let greenDiv = document.createElement("div")
+let green = document.createElement("section")
+let orange = document.createElement("section")
+mainDiv.className = "main-div"
+pink1.className ="pink"
+purple.className ="purple"
+purple2.className ="purple"
+purple3.className ="purple"
+purple4.className ="purple"
+blue.className ="blue"
+blue2.className ="blue"
+green.className ="green"
+orange.className ="orange"
+document.querySelectorAll
+document.body.append(mainDiv)
+mainDiv.appendChild(pinkDiv)
+pinkDiv.appendChild(pink1)
+mainDiv.appendChild(purpleDiv)
+purpleDiv.appendChild(purple)
+purpleDiv.appendChild(purple2)
+purpleDiv.appendChild(purple3)
+purpleDiv.appendChild(purple4)
+mainDiv.appendChild(blueDiv)
+blueDiv.appendChild(blue)
+blueDiv.appendChild(blue2)
+mainDiv.appendChild(greenDiv)
+greenDiv.appendChild(green)
+greenDiv.appendChild(orange)
