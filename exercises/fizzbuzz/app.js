174578ef5397218fa4264fb04ca412601336bab3
Paul D'Amico <pauldamico88@gmail.com>
2022-08-22 22:38:31 -0400

    cleaned up all level1 assignments

--- exercises/fizzbuzz/app.js
+++ exercises/fizzbuzz/app.js
@@ -4,10 +4,10 @@ function phraseTally() {
     buzzArray = []
     let tracker = {fizzbuzz:0, fizz:0, buzz:0,}
   for (let x = 1; x <= 100; x++) {
-    if(x % 3 === 0 && x % 5 ===0){fizzbuzzArray.push("fizzbuzz") /*&& console.log("fizzbuzz")*/}
-    else if (x % 3 === 0){fizzArray.push("fizz") /*&&console.log("fizz")*/}
-    else if (x % 5 === 0){buzzArray.push("buzz") /*&&console.log("buzz")*/}
-    else {/*console.log(x)*/}
+    if(x % 3 === 0 && x % 5 ===0){fizzbuzzArray.push("fizzbuzz") && console.log("fizzbuzz")}
+    else if (x % 3 === 0){fizzArray.push("fizz") &&console.log("fizz")}
+    else if (x % 5 === 0){buzzArray.push("buzz") &&console.log("buzz")}
+    else {console.log(x)}
   }
   tracker.fizzbuzz = fizzbuzzArray.length
   tracker.fizz = fizzArray.length
