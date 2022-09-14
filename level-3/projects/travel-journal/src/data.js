1868aa3ffde5a14441b8445e65be8f349bd2ceb2
Paul D'Amico <pauldamico88@gmail.com>
2022-09-14 11:30:02 -0400

    finished my travel journal

--- level-3/projects/travel-journal/src/data.js
+++ level-3/projects/travel-journal/src/data.js
@@ -1,23 +1,38 @@
 export default [
-    {
-        id:1,
-        title: "Washington D.C.",
-        location: "USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://media.istockphoto.com/photos/united-states-capitol-picture-id182666077?b=1&k=20&m=182666077&s=170667a&w=0&h=4Sl7rV1CUv2wtf6M6__IfmUE8TWzDsOj3wUhssB4PQI="
-    },
-    {
-        id:1,
-        title: "Washington D.C.",
-        location: "USA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://media.istockphoto.com/photos/united-states-capitol-picture-id182666077?b=1&k=20&m=182666077&s=170667a&w=0&h=4Sl7rV1CUv2wtf6M6__IfmUE8TWzDsOj3wUhssB4PQI="
-    }
-    
-]
+  {
+    id: 1,
+    title: "Washington D.C.",
+    location: "USA",
+    googleMapsUrl:"https://www.google.com/maps/place/Washington,+DC/@38.8938003,-77.1581001,12z/data=!3m1!4b1!4m5!3m4!1s0x89b7c6de5af6e45b:0xc2524522d4885d2a!8m2!3d38.9071923!4d-77.0368707",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:"Washington, DC, the U.S. capital, is a compact city on the Potomac River, bordering the states of Maryland and Virginia. It’s defined by imposing neoclassical monuments and buildings – including the iconic ones that house the federal government’s 3 branches: the Capitol, White House and Supreme Court.",
+    imageUrl:
+      "https://media.istockphoto.com/photos/united-states-capitol-picture-id182666077?b=1&k=20&m=182666077&s=170667a&w=0&h=4Sl7rV1CUv2wtf6M6__IfmUE8TWzDsOj3wUhssB4PQI=",
+  },
+  {
+    id: 2,
+    title: "Cancun",
+    location: "Mexico",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Hyatt+Ziva+Cancun/@21.1217801,-86.9192735,12z/data=!3m1!5s0x8f4c28a07419a1a9:0xe2d096aa8cf3470e!4m9!1m2!2m1!1scancun+mexico+resorts!3m5!1s0x8f4c28a07503798b:0x5754071fee7a21aa!5m2!4m1!1i2!15sChVjYW5jdW4gbWV4aWNvIHJlc29ydHOSAQVob3RlbOABAA",
+    startDate: "22 March, 2015",
+    endDate: "28 March, 2015",
+    description:
+      "Cancún, a Mexican city on the Yucatán Peninsula bordering the Caribbean Sea, is known for its beaches, numerous resorts and nightlife. It’s composed of 2 distinct areas: the more traditional downtown area, El Centro, and Zona Hotelera, a long, beachfront strip of high-rise hotels, nightclubs, shops and restaurants.",
+    imageUrl:
+      "https://s3.amazonaws.com/crowdriff-media/full/22bab5ad4b9aa1027ad00a84ea7493d2c0c5e666d43d3b9413e332bdbd3f1780.jpg",
+  },
+  {
+    id: 3,
+    title: "Saint Thomas",
+    location: "USA",
+    googleMapsUrl:"https://www.google.com/maps/place/St+Thomas/@18.3429754,-64.9719512,13z/data=!3m1!4b1!4m5!3m4!1s0x8c051a89dc0635b7:0x5d1280e7f1f697e4!8m2!3d18.3380965!4d-64.8940946",
+    startDate: "22 March, 2015",
+    endDate: "28 March, 2015",
+    description:
+      "Cancún, a St. Thomas is the gateway isle of the U.S. Virgin Islands in the Caribbean. It's known for its beaches and snorkeling spots. Territorial capital Charlotte Amalie, founded by the Danish in the 1600s, is a busy cruise-ship port. Historic buildings include a 1679 watchtower called Blackbeard’s Castle, in reference to the area’s pirate history.  ",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/a0/3d/a0/view-of-the-park.jpg?w=300&h=300&s=1",
+  },
+];
