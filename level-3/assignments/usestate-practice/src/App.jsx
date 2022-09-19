67d4a4be0a80f8c9f550a2eb4ca519a69b7db6b9
Paul D'Amico <pauldamico88@gmail.com>
2022-09-19 12:40:52 -0400

    finished usestate practice assignment

--- level-3/assignments/usestate-practice/src/App.jsx
+++ level-3/assignments/usestate-practice/src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, setState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //   const [color, setColor] = useState("pink")                                    1.
+  // function changeColor (){
+  //   setColor("green")
+  // }
+  const [people, setPeople] = useState([
+                                                                                         //   2.
+    {
+      firstName: "John",
+      lastName: "Smith",
+    },
+  ]);
+
+  function addPerson() {
+    setPeople((prev) => {
+      return [...prev, { firstName: "Paul", lastName: "Damico" }];
+    });
+  }                                                                                     //  3.
+
+  const [color, setColor] = useState(false);
+  function changeColor() {
+    setColor(!color);
+  }
+                                                                                          // 4.
+  //a.
+  const [otherColors, setOtherColors] = useState(["pink", "blue"]);
+
+  // setColors(prevColors => {
+  // 	return [...prevColors, "green"]
+  // })
+  function implicitOtherColors() {
+    setOtherColors((prev) => [...prev, "green"]);
+  }
+
+  //b.
+  const [countObject, setCountObject] = useState({ count: 0 });
+  function implicitCountObject() {
+    setCountObject((prevState) => ( {   count: prevState.count + 1}));
+  }
+
+  const peopleList = people.map((person) => (
+    <li key={person.lastName}>
+      {person.firstName}, {person.lastName}
+    </li>
+  ));
+                                                                                                  // 5.
+  // const [colors, setColors] = useState(["pink", "blue"])
+
+  // setColors("green")   State is a array and this is changing the entire state into the string "green"
+  // it should be the following
+  // setColors(prev=>([...prev, "green"]))
+
+
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {/* <div style={{backgroundColor:color}} className="App"> */}
+      <div
+        style={{ backgroundColor: color ? "pink" : "green" }}
+        className="App"
+      >
+        <button onClick={changeColor}>Change Color</button>
+        <button onClick={addPerson}>Add Person</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {peopleList}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
