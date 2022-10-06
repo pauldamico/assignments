25a719355570368b9a0c7d3ffbe168f4504346fe
Paul D'Amico <pauldamico88@gmail.com>
2022-10-06 19:23:33 -0400

    fixed all callbacks on uglythings.  added react router lessons

--- level-4/assignment/ugly-things/src/components/Edit.jsx
+++ level-4/assignment/ugly-things/src/components/Edit.jsx
@@ -1,99 +1,106 @@
-import React, {useState, useContext} from "react"
-import { UglyContext } from "../uglyContext"
-import axios from "axios"
-export default function Edit (props){
-  const {setUglyList, uglyList, deleteUglyItem} = useContext(UglyContext)
+import React, { useState, useContext } from "react";
+import { UglyContext } from "../uglyContext";
+import axios from "axios";
+export default function Edit(props) {
+  const { setUglyList, uglyList, deleteUglyItem } = useContext(UglyContext);
   const [editThing, setEditThing] = useState({
-    title:"",
+    title: "",
     description: "",
-    imgUrl:
-      ""
-  })
+    imgUrl: "",
+  });
 
+  const editChangeHandler = () => {
+    const { name, value } = event.target;
+    setEditThing((prev) => ({ ...prev, [name]: value }));
 
+    console.log(editThing);
+  };
 
-  const editChangeHandler = ()=>{
-    const {name, value} = event.target
-    setEditThing(prev=>({...prev, [name]:value}))
+  const toggleEditHandler = (id) => {
+    setEditThing((prev) => ({
+      ...prev,
+      title: props.item.title,
+      description: props.item.description,
+      imgUrl: props.item.imgUrl,
+    }));
 
-    console.log(editThing)
-  }
+    axios
+      .put(`https://api.vschool.io/pauldamico/thing/${id}`, editThing)
+      .then(setUglyList((prev) =>
+          prev.map((item) =>
+            id === item._id
+              ? {
+                  ...item,
+                  title: editThing.title,
+                  description: editThing.description,
+                  imgUrl: editThing.imgUrl,
+                  value: !item.value,
+                }
+              : { ...item }
+          )
+        )
+      )
+      .catch((err) => console.log(err));
+    console.log(uglyList);
+  };
 
-const toggleEditHandler = (id) => {
-
-  setEditThing(prev=>({...prev, title:props.item.title, description:props.item.description, imgUrl:props.item.imgUrl}))
-  setUglyList((prev) =>
-    prev.map((item) =>
-      id === item._id
-        ? {
-            ...item,
-            title:editThing.title,
-            description:editThing.description,   
-            imgUrl:editThing.imgUrl,             
-            value: !item.value
-          }
-        : { ...item }
-    )
-  );
-  axios.put(`https://api.vschool.io/pauldamico/thing/${id}`, editThing)
-  .then(res=>res.data)
-  .catch(err=>console.log(err))
-  console.log(uglyList)
-};
-
-    return(<div className="main-edit-input-div">  
-      {props.item.value === true &&
-      <div className="edit-main-div">
-<div>
-  <nav className="edit-nav"><h1>Change Info</h1></nav>
-          <label className="enter-url">Enter URL</label>
-          <input
-            className="url-input"
-            name="imgUrl"
-         value={editThing.imgUrl}
-            type="text"
-            onChange={editChangeHandler}
-        
-          />
+  return (
+    <div className="main-edit-input-div">
+      {props.item.value === true && (
+        <div className="edit-main-div">
+          <div>
+            <nav className="edit-nav">
+              <h1>Change Info</h1>
+            </nav>
+            <label className="enter-url">Enter URL</label>
+            <input
+              className="url-input"
+              name="imgUrl"
+              value={editThing.imgUrl}
+              type="text"
+              onChange={editChangeHandler}
+            />
+          </div>
+          <div>
+            <label>Enter Title</label>
+            <input
+              className="title-input"
+              name="title"
+              value={editThing.title}
+              type="text"
+              onChange={editChangeHandler}
+            />
+          </div>
+          <div>
+            <label>Description</label>
+            <input
+              className="description-input"
+              name="description"
+              value={editThing.description}
+              type="text"
+              onChange={editChangeHandler}
+            />
+          </div>
         </div>
-        <div>
-          <label>Enter Title</label>
-          <input
-            className="title-input"
-            name="title"
-            value={editThing.title}
-            type="text"
-            onChange={editChangeHandler}
-          />
-        </div>
-        <div>      
-          <label>Description</label>
-          <input
-            className="description-input"
-            name="description"
-            value={editThing.description}
-            type="text"
-            onChange={editChangeHandler}
-          />
-        </div></div>}
-        <div className="edit-button-div">
-        <button className="save-button"
-        onClick={() => {
-          toggleEditHandler(props.item._id);
-        }}
-      >
-        {props.item.value ? "Save" : "Edit"}
-      </button>
-      <button
-      className ="remove-button"
-        onClick={() => {
-          deleteUglyItem(props.item._id);
-        }}
-      >
-        Remove
-      </button>
+      )}
+      <div className="edit-button-div">
+        <button
+          className="save-button"
+          onClick={() => {
+            toggleEditHandler(props.item._id);
+          }}
+        >
+          {props.item.value ? "Save" : "Edit"}
+        </button>
+        <button
+          className="remove-button"
+          onClick={() => {
+            deleteUglyItem(props.item._id);
+          }}
+        >
+          Remove
+        </button>
       </div>
-    </div>)
-
-
+    </div>
+  );
 }
--- level-4/assignment/ugly-things/src/uglyContext.jsx
+++ level-4/assignment/ugly-things/src/uglyContext.jsx
@@ -4,58 +4,76 @@ import axios from "axios";
 const UglyContext = React.createContext();
 
 const UglyContextProvider = (props) => {
-  
   const [uglyData, setUglyData] = useState({
-    title:"",
+    title: "",
     description: "",
-    imgUrl:
-      ""
-  })
-const [uglyList, setUglyList] = useState([])
+    imgUrl: "",
+  });
+  const [uglyList, setUglyList] = useState([]);
 
-useEffect(() => {
-  axios
-    .get("https://api.vschool.io/pauldamico/thing")
-    .then(res=>setUglyList(prev=>([...res.data.map(newItems=>({...newItems, value:false}))])))
-    .catch((err) => console.log("hi"));
-}, []);
+  useEffect(() => {
+    axios
+      .get("https://api.vschool.io/pauldamico/thing")
+      .then((res) =>
+        setUglyList((prev) => [
+          ...res.data
+            .map((newItems) => ({ ...newItems, value: false }))
+            .reverse(),
+        ])
+      )
+      .catch((err) => console.log(err));
+  }, []);
 
+  const uglySubmitHandler = (event) => {
+    event.preventDefault();
+    postAPIData(uglyData);
+    setUglyData((prev) => ({
+      ...prev,
+      title: "",
+      description: "",
+      imgUrl: "",
+    }));
+  };
 
-const uglySubmitHandler = (event)=>{
-event.preventDefault()
-postAPIData(uglyData)
-setTimeout(getAPIData, 100)
-setUglyData(prev=>({...prev, title:"", description:"", imgUrl:""}))
-console.log(uglyList)
-}
+  const uglyChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setUglyData((prev) => ({ ...prev, [name]: value }));
+  };
 
-const uglyChangeHandler = (e)=>{
-  const {name, value} = e.target
-  setUglyData(prev=>({...prev, [name]:value}))
-}
-
-const deleteUglyItem=(id)=>{
-  setUglyList(prev=>prev.filter(item=>item._id != id && {...item}))
-  axios.delete(`https://api.vschool.io/pauldamico/thing/${id}`)
-  .then(res=>console.log(res))
-  .catch(err=>console.log(err))
-  
-}
-function postAPIData (postItem){
-  axios.post("https://api.vschool.io/pauldamico/thing/", postItem)
-  .then(res=>res)
-  .catch(err=>console.log(err))
-}
-function getAPIData (){
-  axios.get("https://api.vschool.io/pauldamico/thing/")
-  .then(res=>setUglyList(prev=>([...res.data.map(newItems=>({...newItems, value:false}))])))
-  .catch(err=>console.log(err))
-// console.log(uglyList.map(newItems=>({...newItems, value:false})))
-
-}
+  const deleteUglyItem = (id) => {
+    axios
+      .delete(`https://api.vschool.io/pauldamico/thing/${id}`)
+      .then(
+        setUglyList((prev) =>
+          prev.filter((item) => item._id != id && { ...item })
+        )
+      )
+      .catch((err) => console.log(err));
+  };
+  function postAPIData(postItem) {
+    axios
+      .post("https://api.vschool.io/pauldamico/thing/", postItem)
+      .then((res) =>
+        setUglyList((prev) => {
+          return [{ ...res.data, value: false }, ...prev];
+        })
+      )
+      .catch((err) => console.log(err));
+  }
 
   return (
-    <UglyContext.Provider value={{uglyData:uglyData,  uglyList:uglyList, setUglyList:setUglyList, deleteUglyItem:deleteUglyItem, uglyChangeHandler:uglyChangeHandler, uglySubmitHandler:uglySubmitHandler}}>{props.children}</UglyContext.Provider>
+    <UglyContext.Provider
+      value={{
+        uglyData: uglyData,
+        uglyList: uglyList,
+        setUglyList: setUglyList,
+        deleteUglyItem: deleteUglyItem,
+        uglyChangeHandler: uglyChangeHandler,
+        uglySubmitHandler: uglySubmitHandler,
+      }}
+    >
+      {props.children}
+    </UglyContext.Provider>
   );
 };
 
