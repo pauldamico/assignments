5d5444144b1b0387053386ea8994ee5c0d712e04
Paul D'Amico <pauldamico88@gmail.com>
2022-10-20 14:04:02 -0400

    added portfolio

--- level-4/project/level-4-capstone/src/App.jsx
+++ level-4/project/level-4-capstone/src/App.jsx
@@ -17,13 +17,13 @@ function App() {
 
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => {
           localStorage.clear();
         }}
       >
         Clear Local Storage
-      </button>
+      </button> */}
       <Nav mmoData={mmoData} />
       <Routes>
         <Route
