8680ca6777a2ed444c40c30c788b86b03099ba3d
Paul D'Amico <pauldamico88@gmail.com>
2022-11-03 16:12:01 -0400

    completed bounty hunting with mongoose backend

---
+++ level-5/project-exercises/bounty-hunting-with-mongoose/routes/bountyRouter.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bountyRouter = express.Router();
+const Bounty = require("../models/bounty.js");
+
+// create get, post put, delete : crud
+
+bountyRouter
+  .route("/")
+  .get((req, res, next) => {
+Bounty.find((err, foundItem)=>{
+    if(err){
+        res.status(500)
+        return next(err)
+    }
+res.status(200).send(foundItem)
+})
+  })
+  .post((req, res, next) => {
+    const newBountyInfo = new Bounty(req.body)
+    newBountyInfo.save(req.body, (err, newBounty)=>{
+    if(err){
+        res.status(500)
+        return next(err)
+    }
+    res.send(newBounty)
+})
+  });
+
+bountyRouter.put('/:bountyId', (req, res, next) => {
+Bounty.findOneAndUpdate({_id:req.params.bountyId}, req.body, {new:true}, (err, updatedBounty)=>{
+    if(err){
+        res.status(500)
+        return next(err)
+    }
+    res.send(updatedBounty)
+})
+});
+
+bountyRouter.delete('/:bountyId', (req, res, next) => {
+Bounty.findOneAndDelete({_id:req.params.bountyId}, (err, deletedBounty)=>{
+if(err){
+    res.status(500)
+    return next(err)
+}
+res.send(`The ID of ${deletedBounty._id} has been deleted`)
+})
+});
+
+module.exports = bountyRouter;
---
+++ level-5/project-exercises/bounty-hunting-with-mongoose/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/bountydb", ()=>{console.log("Connected to database")})
+
+app.get("/", (req, res, err)=>{
+    res.send("Welcome to the Bounty Hunter API")
+})
+
+app.use('/bounties', require('./routes/bountyRouter.js'))
+
+app.use((err, req, res,)=>{
+return res.send(err.message)
+})
+
+
+
+
+
+
+app.listen(9000, console.log("Server is running on port 9000"))
